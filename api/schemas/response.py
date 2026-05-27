9edb3423532914a8b8bf71e54f6e852765b7fe81
KauaSd <kauas657@gmail.com>
2026-05-27 15:05:56 -0300

    rota descrição aluno

--- api/schemas/response.py
+++ api/schemas/response.py
@@ -1,2 +1,10 @@
 #Define como sai as respostas da api
 #É usado para não gastar processamento com saidas erradas
+from pydantic import BaseModel
+class AlunoResponse(BaseModel):
+    nome: str
+    idade: int | None
+    serie: str
+    neurodivergencia: str
+    diagnosticado: bool | None
+    descricao: str
